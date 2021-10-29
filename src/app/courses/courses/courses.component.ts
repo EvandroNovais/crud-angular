ed8cb6a75e8636fca37f34338314a816def24cef
Evandro Novais <evandro-novais@outlook.com>
2021-10-29 12:09:28 -0300

    Created and implemented courses.json request

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,6 +2,7 @@ import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -10,17 +11,20 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  //courses: Course[] =[];
+  courses: Observable<Course[]>;
 
   displayedColumns = ['name','category'];
 
   constructor(private coursesService: CoursesService) {
     //this.courses = [];
-    //this.courses = this.coursesService.list();
+    this.courses = this.coursesService.list();
+
+    //this.coursesService.list().subscribe(courses => this.courses = courses);
    }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+
   }
 
 }
